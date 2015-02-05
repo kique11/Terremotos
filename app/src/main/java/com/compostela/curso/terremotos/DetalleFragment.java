1ad6f4159062f76b9fd41cded83b8c2a51740a5d
kqkique <programacioncostas@gmail.com>
2015-02-05 13:43:05 +0100

    Context Menu

--- app/src/main/java/com/compostela/curso/terremotos/DetalleFragment.java
+++ app/src/main/java/com/compostela/curso/terremotos/DetalleFragment.java
@@ -1,11 +1,26 @@
 package com.compostela.curso.terremotos;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by mañá on 02/02/2015.
  */
 public class DetalleFragment extends Fragment{
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
 }
